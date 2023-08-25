295d97c90f5b6704109aae661433c9f42e458e93
miswah <miswah.md@gmail.com>
2023-08-25 21:25:57 +0530

    (enh): Tasks Entity with status and created/completed

--- src/main/java/io/miswah/secondbrain/entity/Tasks.java
+++ src/main/java/io/miswah/secondbrain/entity/Tasks.java
@@ -1,10 +1,13 @@
 package io.miswah.secondbrain.entity;
 
 import io.miswah.secondbrain.constants.Priority;
+import io.miswah.secondbrain.constants.TaskStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @Table(name = "tasks")
@@ -23,6 +26,15 @@ public class Tasks {
 
     @Column(name = "priority")
     private Priority priority;
+
+    @Column(name = "status")
+    private TaskStatus status = TaskStatus.YET_TO_PICK;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "finished_at")
+    private LocalDateTime finishedAt = null;
 
 
     public Tasks(String title, Priority priority) {
