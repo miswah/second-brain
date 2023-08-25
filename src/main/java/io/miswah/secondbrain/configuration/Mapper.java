d5298290808f9fa413015df2606b72c07e232b6d
miswah <miswah.md@gmail.com>
2023-08-25 19:38:22 +0530

    (enh): add mapper for entity to dto conversion

---
+++ src/main/java/io/miswah/secondbrain/configuration/Mapper.java
@@ -0,0 +1,14 @@
+package io.miswah.secondbrain.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Mapper {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
