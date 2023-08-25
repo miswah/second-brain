81fc8c7b9f35b723cb938e4b8f1c64834641e28d
miswah <miswah.md@gmail.com>
2023-08-25 17:53:00 +0530

    (initial): starter files with mariadb driver added

---
+++ src/main/java/io/miswah/secondbrain/SecondBrainApplication.java
@@ -0,0 +1,13 @@
+package io.miswah.secondbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondBrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/miswah/secondbrain/SecondBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package io.miswah.secondbrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
